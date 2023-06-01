138d9fece318a24ddd603bcdbd66b4d34b29143a
Promise-Devotion <sijun0224@msn.com>
2023-06-01 22:17:14 +0800

    提交项目

---
+++ db.py
@@ -0,0 +1,27 @@
+import pymysql
+import pprint
+
+# 创建连接
+
+def get_conn():
+    return pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='sophie123456',
+        database='huamao',
+        charset='utf8'
+    )
+
+def query_data(sql):
+    conn = get_conn()
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        return cursor.fetchall()
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    sql = "SELECT * FROM Persons"
+    datas = query_data(sql)
+    pprint.pprint(datas)
