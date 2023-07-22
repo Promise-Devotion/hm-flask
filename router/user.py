9e7e174f718187fd5ee368d2b11e6ba579dfe1c8
Promise-Devotion <sijun0224@msn.com>
2023-07-22 18:22:32 +0800

    优化用户接查询数据字段

--- router/user.py
+++ router/user.py
@@ -5,7 +5,6 @@ import time
 from flask import request, Blueprint, jsonify
 from common.md5_operate import get_md5
 
-
 from config import SQLManager
 # from flask_restful import Resource, Api
 from db import query_data, update_data
@@ -20,16 +19,16 @@ def user():
         sql = ''
         person_id = request.args.get('id')
         if person_id:
-            sql = f"SELECT * FROM t_user WHERE user_id={person_id}"
+            sql = f"SELECT first_name AS firstname, last_name AS lastname, email AS email, nick_name AS nickname, user_id AS userid, sex AS sex FROM t_user WHERE user_id={person_id}"
         else:
-            sql = "SELECT * FROM t_user"
+            sql = "SELECT first_name AS firstname, last_name AS lastname, email AS email, nick_name AS nickname, user_id AS userid, sex AS sex FROM t_user"
         db = SQLManager()
         datas = db.get_list(sql)
         db.close()
         if len(datas) > 1:
-            response = jsonify({'code': 200, 'data': datas})
+            response = jsonify({'code': 200, 'data': datas, "message": "查询成功!"})
         else:
-            response = jsonify({'code': 200, 'data': datas[0]})
+            response = jsonify({'code': 200, 'data': datas[0], "message": "查询成功!"})
         # response = jsonify(datas)
         return response
     elif request.method == 'POST':
@@ -63,28 +62,31 @@ def user():
     else:
         return 'error'
 
+
 @userapi.route("/login", methods=["post"])
 def user_login():
     """登录用户"""
     email = request.values.get("email", "").strip()
     password = request.values.get("password", "").strip()
-    if email and password: # 注意if条件中空串 "" 也是空, 按False处理
+    if email and password:  # 注意if条件中空串 "" 也是空, 按False处理
         sql1 = "SELECT email FROM t_user WHERE email = '{}'".format(email)
         db = SQLManager()
         res1 = db.get_list(sql1)
         print("查询到用户名 ==>> {}".format(res1))
         if not res1:
+            db.close()
             return jsonify({"code": 1003, "msg": "用户不存在！！！"})
-        md5_password = get_md5(email, password) # 把传入的明文密码通过MD5加密变为密文
+        md5_password = get_md5(email, password)  # 把传入的明文密码通过MD5加密变为密文
         sql2 = "SELECT * FROM t_user WHERE email = '{}' and user_password = '{}'".format(email, password)
         res2 = db.get_list(sql2)
+        db.close()
         print("获取 {} 用户信息 == >> {}".format(email, res2))
         if res2:
             time_stamp = int(time.time())  # 获取当前时间戳
             # token = "{}{}".format(username, timeStamp)
             # username = res2[0].username
             token = get_md5(email, str(time_stamp))  # MD5加密后得到token
-            redis_db.handle_redis_token(email, token) # 把token放到redis中存储
+            redis_db.handle_redis_token(email, token)  # 把token放到redis中存储
             data = {  # 构造一个字段，将 id/username/token/login_time 返回
                 "id": res2[0]["user_id"],
                 "email": email,
